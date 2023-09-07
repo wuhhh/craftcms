6da2dc0e83775ee8f46ad22c12296355c23aa537
Huw Roberts <huwrroberts@gmail.com>
2023-09-07 14:01:13 +0100

    Watch public folder for changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import manifestSRI from 'vite-plugin-manifest-sri';
 import path from 'path';
 import viteCompression from 'vite-plugin-compression';
 import ViteRestart from 'vite-plugin-restart';
+import copy from "vite-plugin-copy-smartly";
 
 // https://vitejs.dev/config/
 export default defineConfig(({command}) => ({
@@ -32,6 +33,11 @@ export default defineConfig(({command}) => ({
         'templates/**/*',
       ],
     }),
+		copy({
+			src: path.resolve(__dirname, "src/public/"),
+			pattern: /\.(svg|png|jpg|gif|webp|json|lottie)$/,
+			dest: path.resolve(__dirname, "web/dist/"),
+		}),
   ],
   publicDir: path.resolve(__dirname, 'src/public'),
   resolve: {
