2e4e3589ad9b176ee4549a1749234fcaf13e33f0
Huw Roberts <huwrroberts@gmail.com>
2023-12-04 09:11:55 +0000

    Use css instead of pcss, add postcss editor config

--- vite.config.js
+++ vite.config.js
@@ -43,7 +43,7 @@ export default defineConfig(({command}) => ({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      '@css': path.resolve(__dirname, 'src/pcss'),
+      '@css': path.resolve(__dirname, 'src/css'),
       '@js': path.resolve(__dirname, 'src/js'),
     },
   },
