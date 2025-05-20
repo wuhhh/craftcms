c842e49fd6d22ac1d644d231b3717a1fa615bcbe
Huw Roberts <huw@huwroberts.dev>
2025-05-20 12:43:54 +0100

    Refactor postcss.config.js to use ES module syntax

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,20 @@
-module.exports = {
+export default {
   plugins: {
     'postcss-import': {},
     '@tailwindcss/postcss': {},
-		'postcss-pxtorem': {
-			rootValue: 16,
-			unitPrecision: 5,
-			propWhiteList: [
-				'*',
-				'!border',
-				'!border-width',
-				'!border-top-width',
-				'!border-right-width',
-				'!border-bottom-width',
-				'!border-left-width',
-			],
-		},
+    'postcss-pxtorem': {
+      rootValue: 16,
+      unitPrecision: 5,
+      propWhiteList: [
+        '*',
+        '!border',
+        '!border-width',
+        '!border-top-width',
+        '!border-right-width',
+        '!border-bottom-width',
+        '!border-left-width',
+      ],
+    },
     'autoprefixer': {}
   },
 };
